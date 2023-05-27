8e9d03acc8b5a33ed05e760b405bc0ca0b343b9b
bktpp <117415722+bktpp@users.noreply.github.com>
2023-05-26 23:26:32 -0500

    .

--- src/Data.js
+++ src/Data.js
@@ -14,7 +14,6 @@ export const socialsLinks = [
   { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
   { id: 3, href: "https://www.squarespace.com", icon: "fab fa-squarespace" },
 ];
-
 export const services = [
   {
     id: 1,
